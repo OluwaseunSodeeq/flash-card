dba5fb1cc74f14f2ed535bf5a7d6134e2f592b0f
Oluwaseun Sodeeq <ademolaoluwaseun90@gmail.com>
2024-08-15 14:32:23 +0100

    first commit

---
+++ app/page.js
@@ -0,0 +1,6 @@
+import { Box } from "@mui/material";
+// import Image from "next/image";
+
+export default function Home() {
+  return <Box>Hello</Box>;
+}
---
+++ firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAhkFYG0vYRi0t0NoBjMAgbdg4OJS0jB-w",
+  authDomain: "flashcardapp-de5c2.firebaseapp.com",
+  projectId: "flashcardapp-de5c2",
+  storageBucket: "flashcardapp-de5c2.appspot.com",
+  messagingSenderId: "177095540463",
+  appId: "1:177095540463:web:27dad39b210d46fb2fa51a",
+  measurementId: "G-BEQW7QNSMY",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
